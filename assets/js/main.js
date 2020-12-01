e018491bdb566482e53b045952288063504b4f3f
limunosekai <powerlsh0103@gmail.com>
2020-12-01 14:18:28 +0900

    Add localStorage save func & Delete button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const deleteItems = document.querySelector('.delete-items');
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
-  e.preventDefault(); // page reloading을 막아줌
+  e.preventDefault(); // submit event의 page reloading을 막아줌
   const text = (this.querySelector('[name=item]')).value;
   const item = {
     text,       // ES6 key값과 value값이 같을 경우 생략 가능
@@ -12,6 +13,7 @@ function addItem(e) {
 
   items.push(item);
   populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));  //localStorage에 key & value 값으로 저장
   this.reset();
 }
 
@@ -24,7 +26,24 @@ function populateList(plates = [], platesList) {  //함수 파라미터 값에 d
       </li>
     `
   }).join('');
-  console.log(platesList);
+}
+
+function toggleDone(e) {
+  if(!e.target.matches('input')) return; // checkbox 영역 이외의 부분을 클릭하면 escape
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+function removeItem(e) {
+  localStorage.clear();
+  populateList(items, itemsList);
 }
 
 addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone); // 이벤트의 위임, li를 감싸고 있는 ul에 이벤트를 건다.
+deleteItems.addEventListener('submit', removeItem);
+
+populateList(items, itemsList);
